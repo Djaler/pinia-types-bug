cdbd06274e45c585d2b72752b170bd01ca6e30d6
Kirill Romanov <djaler1@gmail.com>
2021-11-23 14:24:51 +0300

    Install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueCompositionAPI from '@vue/composition-api';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 
 Vue.use(VueRouter);
 Vue.use(VueCompositionAPI);
+Vue.use(PiniaVuePlugin);
 
 const router = new VueRouter({});
 
+const pinia = createPinia();
+
 new Vue({
     router,
+    pinia
 });
